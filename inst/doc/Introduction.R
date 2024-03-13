c1e479b2ba3ac94871a04bccce1437c44d9cfbf3
Julian Busch <jb@quants.ch>
2024-03-13 11:40:05 +0000

    version 1.0.0

--- inst/doc/Introduction.R
+++
@@ -1,6 +0,0 @@
-## ---- eval=FALSE--------------------------------------------------------------
-#  ReferenceManual(pkg = "Rd2md", outdir = getwd())
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  ReferenceManual(pkg = "Rd2md", outdir = getwd())
-
---
+++ inst/doc/introduction.R
@@ -0,0 +1,12 @@
+## ----eval=FALSE---------------------------------------------------------------
+#  render_refman()
+
+## ----eval=FALSE---------------------------------------------------------------
+#  render_refman(
+#    pkg = "path/to/package_root",
+#    output_file = "path/to/output/reference_manual.md"
+#  )
+
+## ----eval=FALSE---------------------------------------------------------------
+#  render_refman(pkg = "<pkg-name>")
+
